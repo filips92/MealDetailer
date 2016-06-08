501c4c66c907108ab969680fd6d83f614c2211bf
Krzysztof Zbiciński <fistasheq+github@gmail.com>
2016-06-08 18:34:43 +0200

    recalculating summary

--- MealDetailer/MealDetailer/Scripts/app.js
+++ MealDetailer/MealDetailer/Scripts/app.js
@@ -1,8 +1,8 @@
 ﻿angular.module('MealDetailer', ['ui.bootstrap']);
 
-angular.module('MealDetailer').controller('SearchController', function($scope, $http, $rootScope) {
+angular.module('MealDetailer').controller('SearchController', function ($scope, $http, $rootScope) {
 
-    $scope.doSearch = function() {
+    $scope.doSearch = function () {
         if ($scope.query && $scope.query.length > 2) {
             $http.get('/Home/QueryDatabase?query=' + $scope.query).then(function (response) {
                 var parsedResponse = response.data;
@@ -14,8 +14,8 @@ angular.module('MealDetailer').controller('SearchController', function($scope, $
             });
         }
     }
-    
-    $scope.addProduct = function(ndbno) {
+
+    $scope.addProduct = function (ndbno) {
         $rootScope.$emit('addProduct', ndbno);
     }
 });
@@ -24,21 +24,42 @@ angular.module('MealDetailer').controller('MainController', function ($scope, $h
     $scope.products = [];
 
     $rootScope.$on('addProduct', onAddProduct, handleError);
-    
-    $scope.save = function() {
-        $http.post('/Home/SaveReport', $scope.report).then(function(response) {
+
+    $scope.recalculateSummary = function () {
+        var result = [];
+        var nutrientsAmount = $scope.products.length ? $scope.products[0].nutrients.length : 0;
+
+        for (var i = 0; i < $scope.products.length; i++) {
+            var singleProduct = $scope.products[i];
+            for (var j = 0; j < nutrientsAmount; j++) {
+                var singleNutrient = singleProduct.nutrients[j];
+                if (i === 0) {
+                    result.push({
+                        name: singleNutrient['@name'],
+                        amountInRecipe: 0
+                    });
+                }
+                result[j].amountInRecipe += parseFloat(singleNutrient['@value']) * singleProduct.weight / 100;
+            }
+        }
+
+        $scope.summary = result;
+    }
+
+    $scope.save = function () {
+        $http.post('/Home/SaveReport', $scope.report).then(function (response) {
             alert('ok');
         }, handleError);
     }
 
     onAddProduct({}, '42291'); // add peanut butter to begin with
 
-    function handleError (error) {
+    function handleError(error) {
         console.log(error);
     }
 
-    function onAddProduct (event, ndbno) {
-        $http.get('/Home/GetFoodReport?id=' + ndbno).then(function(response) {
+    function onAddProduct(event, ndbno) {
+        $http.get('/Home/GetFoodReport?id=' + ndbno).then(function (response) {
             var product = transformReportResponse(response);
             $scope.products.push(product);
         });
